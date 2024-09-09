838e64dc741df4e15720c8a4a90d314017d04e7b
AlexanderBraatz <alex.braatz.g@gmail.com>
2024-09-09 13:36:31 +0100

    feat: add colour palette and remove unnecessary files and code

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,35 @@
-import { useState } from 'react';
 import { styled } from 'styled-components';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import './assets/fonts/fonts.css';
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
-		<>
-			<div>
-				<a
-					href="https://vitejs.dev"
-					target="_blank"
-				>
-					<img
-						src={viteLogo}
-						className="logo"
-						alt="Vite logo"
-					/>
-				</a>
-				<a
-					href="https://react.dev"
-					target="_blank"
-				>
-					<img
-						src={reactLogo}
-						className="logo react"
-						alt="React logo"
-					/>
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount(count => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
+		<OuterContainer id="OuterContainer">
+			<BodyContainer>
+				<Title>Is it the T'ing ?</Title>
+			</BodyContainer>
+		</OuterContainer>
 	);
 }
+
+const Title = styled.h1`
+	background-color: red;
+
+	height: 100px;
+	font-size: 6rem;
+	font-weight: 100;
+`;
+const OuterContainer = styled.div`
+	width: 100vw;
+	height: 100vh;
+	background-color: red;
+	background-color: ${props => props.theme.desktop.white};
+`;
+const BodyContainer = styled.div`
+	max-width: 107.8rem;
+	height: 100vh;
+	background-color: ${props => props.theme.desktop.grey};
+
+	margin: auto;
+`;
 export default App;
---
+++ src/assets/colors/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+	white: '#ffffff',
+	grey: '#F0F0F0',
+	purple_dark: '#493C80',
+	purple_medium: '#8582C3',
+	purple_light: '#C9C8E1',
+	purple_button: '#423DA4'
+};
+
+export default colors;
